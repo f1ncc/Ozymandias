5511731776749f6a169791b691b06b359f4ed29f
Sathya LK <rajavslk@gmail.com>
2020-06-09 22:57:34 +0530

    feature: Search by name

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
 import { Helmet } from "react-helmet";
-// import Search from "./components/Search";
+import Search from "./components/Search";
 
 function App() {
   const [characters, setChar] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  // const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("");
 
   useEffect(() => {
-    fetch(`https://www.breakingbadapi.com/api/characters/`)
+    fetch(`https://www.breakingbadapi.com/api/characters?name=${term}`)
       .then((res) => res.json())
       .then((data) => {
         setChar(data);
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  });
+  }, [term]);
 
   return (
     <div className="container mx-auto py-4">
       <Helmet>
         <style>{"body { background-color: black; }"}</style>
       </Helmet>
-      {/* <Search searchText={(text) => setTerm(text)} />
+      <Search searchText={(text) => setTerm(text)} />
       {!isLoading && characters.length === 0 && (
         <h1 className="text-5xl text-center mx-auto mt-32 text-white">
           No characters with that name
         </h1>
-      )} */}
+      )}
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32 text-white">
           Hang on while it's loading..
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -15,10 +15,10 @@ const Search = ({ searchText }) => {
             onChange={(e) => setText(e.target.value)}
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
-            placeholder="Search for an Character.."
+            placeholder="Search for a Character.."
           />
           <button
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded mr-1"
             type="submit"
           >
             Search
